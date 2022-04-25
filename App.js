56ce1c6d1d94d8ec2b98e0d7e02d7d1a14de2a9c
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-04-25 16:08:50 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import {createStackNavigation} from "@react-navigation/stack";
+import {NavigationContainer,DefaultThem} from '@react-navigation//native';
+import {useFonts} from 'expo-font';
+import Home from './screens/Home'
+import Details from "./screens/Details";
+const Stack = createStackNavigation();
+const theme = {
+  ...DefaultThem,
+  color:{
+    ...DefaultThem.colors,
+    background:'transparent'
+  }
+
+  
+}
+const App =() => {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular :require ("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf")
+
+
+  });
+  if(!loaded) return null
+  return (
+   <NavigationContainer theme={theme}>
+     <Stack.Navigator screenOptions={{headerShown:false}}
+     initialRouteName='Home'>
+       <StackFrame.Screen name="Home" component={Home}/>
+       <StackFrame.Screen name="Details" component={Details}/>
+     </Stack.Navigator>
+
+   </NavigationContainer>
+  );
+}
+export default App;
+
